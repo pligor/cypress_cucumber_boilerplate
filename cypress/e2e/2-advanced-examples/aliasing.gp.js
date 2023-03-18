a6de69e6040caae509fa17c85f07834d9ed11f27
GP <pligor.george@gmail.com>
2023-03-18 18:05:27 +0200

    aliasing works ok for urls as well

--- cypress/e2e/2-advanced-examples/aliasing.gp.js
+++ cypress/e2e/2-advanced-examples/aliasing.gp.js
@@ -2,11 +2,12 @@
 
 context("Aliasing", () => {
   beforeEach(() => {
-    cy.visit("https://play1.automationcamp.ir/forms.html");
+    cy.visit("https://play1.automationcamp.ir/");
   });
 
   it(".as() - alias a DOM element for later use", () => {
     // https://on.cypress.io/as
+    cy.contains("Forms").parents(".card").find(".btn").click();
 
     // Alias a DOM element for use later
     // We don't have to traverse to the element
@@ -28,15 +29,26 @@ context("Aliasing", () => {
       .should("be.visible");
   });
 
-  it('.as() - alias a route for later use', () => {
-    // Alias the route to wait for its response
-    cy.intercept('GET', '**/comments/*').as('getComment')
+  it(".as() - alias a route for later use", () => {
+    // cy.visit('https://example.cypress.io/commands/aliasing')
 
-    // we have code that gets a comment when
-    // the button is clicked in scripts.js
-    cy.get('.network-btn').click()
+    // cy.intercept('GET', '**/comments/*').as('getComment')
 
-    // https://on.cypress.io/wait
-    cy.wait('@getComment').its('response.statusCode').should('eq', 200)
-  })
+    // // we have code that gets a comment when
+    // // the button is clicked in scripts.js
+    // cy.get('.network-btn').click()
+
+    // // https://on.cypress.io/wait
+    // cy.wait('@getComment').its('response.statusCode').should('eq', 200)
+
+    cy.visit("https://resttesttest.com/");
+
+    // // Alias the route to wait for its response
+    cy.intercept("GET", "**/get").as("getAjax");
+
+    cy.get("button#submitajax").click();
+
+    // // https://on.cypress.io/wait
+    cy.wait("@getAjax").its("response.statusCode").should("eq", 200);
+  });
 });
