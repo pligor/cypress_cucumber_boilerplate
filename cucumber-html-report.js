9ca050317379464d238fb21be4df620f9feef440
GP <pligor.george@gmail.com>
2023-05-20 15:29:50 +0300

    we have added the ability to generate a more nice html report based on the json export of badeball

---
+++ cucumber-html-report.js
@@ -0,0 +1,28 @@
+const  report = require("multiple-cucumber-html-reporter");
+
+report.generate({
+    jsonDir: "reports/json/",
+    reportPath: "./reports/html/multiple-html-report/",
+    displayReportTime: true,
+    openReportInBrowser: true,
+    displayDuration: true,
+    metadata: {
+        browser: {
+            name: "Placeholder",
+            version: "Placeholder",
+        },
+        device: "Placeholder",
+        platform: {
+            name: "Placeholder",
+        },
+    },
+        reportName: "Cypress suite run",
+        customData: {
+            title: "Run info",
+            data: [
+                {label: "Project", value: "Cypress daily run"},
+                {label: "Environment", value: "Local"},
+                {label: 'Execution Date', value: new Date().toUTCString()},
+            ],
+    },
+})
